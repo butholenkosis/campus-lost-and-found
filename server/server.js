d495153ef303ee3d19242cc547aac6a542510097
butholenkosis <butholenkosis320@gmail.com>
2026-04-22 22:47:25 -0400

    Add complete React frontend

--- server/server.js
+++ server/server.js
@@ -1,3 +1,3 @@
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
